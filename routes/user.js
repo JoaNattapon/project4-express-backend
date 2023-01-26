2b1a599b65c632f72444ee469751fae1c775f16b
JoaNattapon <joenattapon.sangchai@gmail.com>
2023-01-26 19:53:33 +0700

    Let's build

--- routes/user.js
+++ routes/user.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const ctrl = require("../controllers");
 
 router.get("/", ctrl.user.test);
+router.post("/signup", ctrl.user.signup);
+router.post("/login", ctrl.user.login);
+router.put("/edit", ctrl.user.edit);
 
 module.exports = router;
